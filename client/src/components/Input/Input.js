2ddb466a20730e51909d9da61940d4ede0968a8b
hyunjeong kim <hyunjeong.kim@biomind.ai>
2020-10-11 13:40:03 +0800

    feature: modify send button style as send image icon

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import './Input.css';
+import "./Input.css";
+import sendIcon from "../../icons/send.png";
 
 const Input = ({ setMessage, sendMessage, message }) => (
-  <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="Type a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-    />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
-  </form>
-)
+    <form className="form">
+        <input
+            className="input"
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={({ target: { value } }) => setMessage(value)}
+            onKeyPress={(event) =>
+                event.key === "Enter" ? sendMessage(event) : null
+            }
+        />
+        {/* <button className="sendButton" onClick={e => sendMessage(e)}>Send</button> */}
+        <label className="sendButton" for="sendbtn">
+            <input type="button" id="sendbtn" onClick={(e) => sendMessage(e)} />
+            <span className="sendButtonSpan">
+                <img
+                    className="sendButtonImg"
+                    alt="Online Icon"
+                    src={sendIcon}
+                />
+            </span>
+        </label>
+    </form>
+);
 
 export default Input;
